3fb9de055de4342991c22509105b649d6380a804
insuanet <insuaminca@insuaminca.com>
2025-04-29 15:00:54 -0400

    Deploy server local

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,12 +16,19 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 // export const API_FAST ='http://127.0.0.1:8000';
 
 // ACCESO EXTERNO INSUAMINCA
-export const PROTEO_URL_ALONE = 'http://186.167.69.10:50080/practica/';
-export const API_URL = 'http://186.167.69.10:50080/practica/api/ptprv/post/';
-export const API_URL2 = 'http://186.167.69.10:50080/practica/api/ptprv/';
-export const PROTEO_URL = 'http://186.167.69.10:50080/practica/api/ptprv/';
-export const API_FAST ='http://127.0.0.1:8000';
+// export const PROTEO_URL_ALONE = 'http://186.167.69.10:50080/practica/';
+// export const API_URL = 'http://186.167.69.10:50080/practica/api/ptprv/post/';
+// export const API_URL2 = 'http://186.167.69.10:50080/practica/api/ptprv/';
+// export const PROTEO_URL = 'http://186.167.69.10:50080/practica/api/ptprv/';
+// export const API_FAST ='http://127.0.0.1:8000';
 
+
+// ACESSO DE SERVER LOCAL
+export const PROTEO_URL_ALONE = 'http://192.168.1.48/';
+export const API_URL = 'http://192.168.1.48/proteoerp/api/ptprv/post/';
+export const API_URL2 = 'http://192.168.1.48/proteoerp/api/ptprv/';
+export const PROTEO_URL = 'http://192.168.1.48/proteoerp/api/ptprv/';
+export const API_FAST ='http://1http://192.168.1.48:5000';
 
 
 export const appConfig: ApplicationConfig = {
