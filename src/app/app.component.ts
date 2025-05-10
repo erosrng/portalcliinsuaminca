167afeaceadee67be59b9634c55cb9980f521cff
jacmnova <josealfre14@gmail.com>
2025-05-10 13:16:24 -0400

    upload ajustes ngnx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AlertasComponent} from "./components/alertas/alertas.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ],
+    imports: [RouterOutlet, AlertasComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
