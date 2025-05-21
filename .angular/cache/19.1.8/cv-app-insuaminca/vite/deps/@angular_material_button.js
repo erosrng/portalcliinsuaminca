dfd7a88d0103549a13e1fa37de779d2dcc71f836
Eros Rangel <eros.rangel444@gmail.com>
2025-05-21 10:44:50 -0400

    Carrusel de productos

---
+++ .angular/cache/19.1.8/cv-app-insuaminca/vite/deps/@angular_material_button.js
@@ -0,0 +1,37 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-SCXE7ZFY.js";
+import "./chunk-HTB6ARLV.js";
+import "./chunk-2O4WY5GE.js";
+import "./chunk-LLW5IOA3.js";
+import "./chunk-YV7PVOCW.js";
+import "./chunk-J2BFM4J6.js";
+import "./chunk-HRMWXWEU.js";
+import "./chunk-ZXNQQMV3.js";
+import "./chunk-P6U2JBMQ.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
